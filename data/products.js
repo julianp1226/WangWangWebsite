40297d05e2d0280d3f35f905d73fb72844ca37a7
Andrew B <88594395+game-sampler@users.noreply.github.com>
2024-02-16 15:07:13 -0500

    Update products.js

--- data/products.js
+++ data/products.js
@@ -4,6 +4,21 @@ import { users } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 import bcrypt from "bcryptjs";
 
+import {
+  validId,
+  validStr,
+  validUsername,
+  validNumber,
+  validState,
+  validZip,
+  validTime,
+  validTimeInRange,
+  validEmail,
+  validExpLevel,
+  validImageUrl,
+  checkPassword
+} from "../validation.js";
+
 const createProduct = async (
   name,
   description,
@@ -21,4 +36,24 @@ const createProduct = async (
     !name || !description || !actualPrice || !discountedPrice || !clinicId || !packageCategoryId
   ) {
     throw "Error: Some necessary inputs not provided";
+  };
+  if (!image) {
+    image = "/public/images/No_Image_Available.jpg";
+  } else {
+    image = validImageUrl(image);
+  };
+    if (!couponId) {
+    couponId = "";
+  } else {
+    couponId = validStr(couponId, "Coupon ID");
+  };
+  //do more for optional args
+  try {
+    name = validStr(name, "Name");
+    description = validStr(description, "Description");
+    actualPrice = validNumber(actualPrice, "Actual Price");
+    discountedPrice = validNumber(actualPrice, "Actual Price");
+    clinicId = validStr(clinicId, "Clinic ID");
+    packageCategoryId = validStr(packageCategoryId, "Package Category ID");
   }
+  //mongoshit
