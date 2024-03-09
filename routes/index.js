ac76cd7ae931de01d0994a454a7ad724fe4d625d
julianp1226 <julianperez122601@gmail.com>
2024-03-09 12:34:37 -0500

    postId route added, feed route created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 import userRoutes from "./users.js";
 import authRoutes from "./auth.js";
+import postRoutes from './posts.js'
 
 const constructor = (app) => {
   app.use("/", authRoutes);
-  app.use("/user", userRoutes); // TODO: Handle own profile, other user profiles, reviews, etc.
+  app.use("/user", userRoutes);
+  app.use("/feed", postRoutes);// TODO: Handle own profile, other user profiles, reviews, etc.
 
   app.get("/", async (req, res) => {
     if (!req.session || !req.session.user || !req.session.user.id) {
